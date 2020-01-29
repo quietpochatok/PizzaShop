c8260510b730436937bcb29ab96145df3755dee6
quietpochatok <bosroin@gmail.com>
2020-01-30 02:33:01 +0500

    play with localStorage & 1-st work console in GC

--- public/1.js
+++ public/1.js
@@ -1,4 +1,8 @@
 function something() 
 {
-		alert ('Hello everybody!!!');
+	let messageForUser = window.localStorage.getItem('message');
+
+		window.localStorage.setItem('message', 22356);
+		
+			alert (messageForUser);
 }
