a7f0b7aea9dbe2ff522ef3016f918705813837e7
quietpochatok <bosroin@gmail.com>
2020-01-30 05:13:08 +0500

    save inf  when close brouser

--- public/1.js
+++ public/1.js
@@ -1,8 +1,9 @@
 function something() 
 {
 	let messageForUser = window.localStorage.getItem('message');
+		 messageForUser = messageForUser*1 + 1;
 
-		window.localStorage.setItem('message', 22356);
+		window.localStorage.setItem('message', messageForUser);
 		
 			alert (messageForUser);
 }
