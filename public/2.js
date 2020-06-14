bf938a5379faf74a7e19d3cee9474106c42f457b
quietpochatok <bosroin@gmail.com>
2020-06-14 20:30:41 +0500

    feat: add order form on page

--- public/2.js
+++ public/2.js
@@ -26,6 +26,22 @@ function total_pizza_in_cart()
 	return cnt;
 }
 
+function order_pizza_in_cart() 
+{
+	let orders = '';
+		for (var i = 0; i < window.localStorage.length; i++) 
+		{
+			let keyNumberPizza = window.localStorage.key(i);//получаем ключ пиццы
+			let amountPizza = window.localStorage.getItem(keyNumberPizza); //получаем значение (hh[key]=x)
+			if (keyNumberPizza.indexOf('prod') == 0) 
+			{
+				orders = orders + keyNumberPizza + '=' + amountPizza ',';
+			}
+		}
+	return orders;
+}
+
+
 function something() 
 {
 	let messageForUser = window.localStorage.getItem('message');
