7b2ecde7d52f1c7e34a6a9ce510dc74c2c5f6e40
quietpochatok <bosroin@gmail.com>
2020-06-18 19:23:17 +0500

    feat: add message 'cart is empty'

--- app.rb
+++ app.rb
@@ -30,15 +30,23 @@ post '/place_order' do
 end
 
 post '/cart' do
+  # получаем список параметров и разбираем их
 	@orders_input = params[:orders_input]
 	@items = parse_orders_input @orders_input
 
+  #выводим сообщение о том, что корзина пуста
+  if @items.length == 0
+    return erb :cart_is_empty
+  end
+    
+  end
+  # выводим список продуктов в корзине
 	@items.each do |item|
 
 		item[0] = Product.find(item[0])
 	end
 
-
+ # возвращаем представление по дефолту
 	erb :cart
 end
 
